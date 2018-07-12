121fa6f39d834d1c2596e0420f09324539a7d4fa
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-07-13 00:28:36 +0800

    0712 這樣可以讓狀態列透明嗎？

--- www/script.js
+++ www/script.js
@@ -1,5 +1,7 @@
 ready = function () {
     try {
+        //setStatusBarTransparent(true);
+        
         window.plugins.intent.setNewIntentHandler(function (intent) {}, function (e) {});
 
         window.plugins.intent.getCordovaIntent(function (intent) {
